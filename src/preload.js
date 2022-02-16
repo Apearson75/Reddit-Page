43614b5a6faa1912875d61cd15c1d9b7976d5987
Phoneguytech75 <88546046+Phoneguytech75@users.noreply.github.com>
2022-02-16 11:58:19 +0000

    updated things

--- src/preload.js
+++ src/preload.js
@@ -1,14 +1,18 @@
-const list = ['memes', 'woooosh']
+
 
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
+    const list = ['memes', 'woooosh']
+    const randomReddit = list[Math.floor(Math.random() * 2)]
+    console.log(randomReddit)    
+    
     const replaceText = (selector, text) => {
       const element = document.getElementById(selector)
       if (element) element.innerText = text
     }
-
-    fetch("https://meme-api.herokuapp.com/gimme/memes")
+    
+    fetch(`https://meme-api.herokuapp.com/gimme/${randomReddit}`)
         .then(res => res.json())
         .then(data => {
             console.log(data);
